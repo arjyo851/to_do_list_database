d350d05b26d58076ce0896581872bb14480b5ca3
arjyo851 <arjyo77@gmail.com>
2021-10-26 20:08:26 +0530

    changes to files

--- app.js
+++ app.js
@@ -147,7 +147,7 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8000;
 }
-app.listen(port);
+
 
 app.listen(port, function () {
     console.log("Server started from port 3000");
