d8931a37bf9cfbe8f29d5eaae6c3919e8b055207
arjyo851 <arjyo77@gmail.com>
2021-10-26 20:26:56 +0530

    hided password

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 // const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
+require("dotenv").config();
 
 const app = express();
 // let items = ["DSA","Badminton","gym"];
